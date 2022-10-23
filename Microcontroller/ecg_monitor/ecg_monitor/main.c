3e53f8f91472d4835188dcb09abb55af7092f4df
CoolD69 <parthasingharoy166@gmail.com>
2022-10-23 22:57:36 +0530

    initial commit

---
+++ Microcontroller/ecg_monitor/ecg_monitor/main.c
@@ -0,0 +1,18 @@
+/*
+ * ecg_monitor.c
+ *
+ * Created: 23-10-2022 22:49:39
+ * Author : 
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+

d3b6bbceece2ff21cbdcd56cc248999096c1ba0c
CoolD69 <parthasingharoy166@gmail.com>
2022-10-26 11:38:03 +0530

    ADC added

--- Microcontroller/ecg_monitor/ecg_monitor/include/src/main.c
+++ Microcontroller/ecg_monitor/ecg_monitor/include/src/main.c
@@ -9,39 +9,55 @@
 #define F_CPU 8000000UL	
 #include <avr/io.h>
 #include <util/delay.h>
-#include "uart.h"
-#include "LiquidCrystal_LCD.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <avr/interrupt.h>
+#include "ADC.h"
+#include "uart.h"
+#include "LiquidCrystal_LCD.h"
 
-char val;
+//Global variable
+int adc_value_int=0;
+char adc_value_char[10];
+
 
 
 
 int main()
 {
-	DDRA = 0x0F;
+	// enable global interrupt
+	sei();
+	//DDRA = 0x04;
+	//Initialization all the init function
 	UART_init(9600);
 	LCD_Init();
+	ADC_Init();
+	
 	
 	while(1)
 	{
-		PORTA = PORTA | (1<<3);
+		//PORTA = PORTA | (1<<3);
 		LCD_Clear();
-		LCD_String("LED ON");
-		LCD_String(45);
+		LCD_String("ADC");
 		
-		
-		
-		UART_SendString("Blue tooth ");
-		_delay_ms(2000);
-		PORTA = PORTA & (~(1<<3));
-		
-		LCD_Clear();	
-		LCD_String("LED OFF");
-		
-		
+		//adc read
+		adc_value_int=ADC_Read(0);
+		itoa(adc_value_int,adc_value_char,10);
+		UART_SendString(adc_value_char);
+		LCD_String_xy(2,1,adc_value_char);	
 		
 		_delay_ms(2000);
+		
 	}
 }
+
+//ADC Lib is interrupt based
+
+
+
+
+
+
+
+
+
